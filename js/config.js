464b8ff9e80ace16fd66f60cb8889db63450e0d9
shiftbyjeanduplessis <shiftbyjeanduplessis@gmail.com>
2026-03-30 21:29:03 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.HEARTY_CONFIG = {
   SUPABASE_URL: "https://xfhflvzkpwycghwbkuld.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_bvxBv7XPZ2i4C-t0DfuYLQ_PM1i0bDq"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmaGZsdnprcHd5Y2dod2JrdWxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NjU3ODAsImV4cCI6MjA5MDE0MTc4MH0.rLvKcuPDrkQFwpbtxHDhyB2yLFJ-f5DcqNv-eIOuZMI"
 };
